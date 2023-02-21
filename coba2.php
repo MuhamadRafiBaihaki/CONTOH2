161db48e78b5a5b0346b1b5f1467154b1702a366
MuhamadRafiBaihaki <rafibaihaki33@gmail.com>
2023-02-21 15:02:31 +0700

    ini contoh

---
+++ coba2.php
@@ -0,0 +1,11 @@
+<?php
+    $ar_buah = ['pepaya', 'mangga', 'pisang', 'jambu'];
+
+    foreach( $ar_buah as $buah){
+        echo $buah;
+    }
+
+?>
+
+
+
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    $nama = 'Muhamad Rafi Baihaki';
+    $umur = 22;
+    $berat = 50;
+
+    echo 'Nama saya : ' .   $nama;
+    echo '<br>Umur saya : ' .$umur;
+    echo '<br>Berat saya : '.$berat;
+?>
+
